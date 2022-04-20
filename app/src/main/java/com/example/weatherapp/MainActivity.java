e5da110059c8b18a7297b252a229a4103dbdaa2b
sonishere <son.nguyentruongwork@gmail.com>
2022-04-21 01:14:58 +0700

    final

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,17 +19,22 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView city;
+    EditText city;
     TextView statusText;
     TextView wind;
     TextView humid;
-
+    TextView temp;
+    TextView main;
+    TextView cityName;
+    TextView pressure;
     ImageView status;
 
     private final String url = "https://api.openweathermap.org/data/2.5/weather";
@@ -41,24 +49,56 @@ public class MainActivity extends AppCompatActivity {
         wind = findViewById(R.id.wind);
         humid = findViewById(R.id.humid);
         status = findViewById(R.id.status);
+        temp = findViewById(R.id.temp);
+        pressure = findViewById(R.id.pressure);
+        cityName = findViewById(R.id.cityName);
+        main = findViewById(R.id.main);
 
+        getWeatherInfo();
     }
 
     @SuppressLint("SetTextI18n")
-    public void getWeatherInfo(View view) {
-        String tempUrl = "";
-        String name = city.getText().toString().trim();
+    public void getWeatherInfo() {
+        city.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    String tempUrl = "";
+                    String name = city.getText().toString().trim();
+                    System.out.println(tempUrl);
+                    if (city.equals("")) {
+                        statusText.setText("City name cannot be emptied");
+                    } else {
+                        tempUrl = url + "?q=" + name + "&appid=" + key;
+                    }
+                    setInfo(tempUrl);
+                }
+                return false;
+            }
+        });
 
-        if (city.equals("")) {
-            statusText.setText("City name cannot be emptied");
-        } else {
-            tempUrl = url + "?q=" + city + ",&appid=" + key;
-        }
+    }
 
+    public void setInfo(String tempUrl) {
         StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
             try {
-                JSONObject jsonRespone = new JSONObject(response);
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                statusText.setText(jsonObjectWeather.getString("description"));
+                main.setText(jsonObjectWeather.getString("main"));
 
+                temp.setText(String.format("%.1f", jsonObjectMain.getDouble("temp") - 273.15)+"°");
+                pressure.setText(Integer.toString(jsonObjectMain.getInt("pressure"))+"hPA");
+                humid.setText(Integer.toString(jsonObjectMain.getInt("humidity"))+"%");
+                String img = "https://openweathermap.org/img/w/" + jsonObjectWeather.getString("icon") + ".png";
+                Picasso.get().load(img).into(status);
+
+                JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                wind.setText(jsonObjectWind.getString("speed")+"m/s");
+
+                cityName.setText(jsonResponse.getString("name"));
             }
             catch (JSONException e) {
                 e.printStackTrace();
@@ -75,6 +115,4 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
     }
-
-
 }
